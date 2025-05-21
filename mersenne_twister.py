dfc6a9dc6faf2364a8dbb0c502ed32d7b23c56cf
msadlej <michal.sadlej@gmail.com>
2025-05-21 17:23:29 +0200

    add mersenne-twister

---
+++ mersenne_twister.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import random
+import sys
+
+
+def getMersenneTwister(n_samples: int) -> list:
+    random.seed(42)
+    return [random.random() for _ in range(n_samples)]
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python mersenne_twister.py {n_samples}")
+        sys.exit(1)
+
+    n_samples = int(sys.argv[1])
+    mersenne_twister = getMersenneTwister(n_samples)
+
+    df = pd.DataFrame(mersenne_twister, columns=["Mersenne-Twister Numbers"])
+    df.to_csv("data/mersenne_twister.csv", index=False)
