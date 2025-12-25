41739d8b1732d713f2f524815b6cfcd55e8d620d
msadlej <michal.sadlej@gmail.com>
2025-12-26 00:09:22 +0100

    Add data

--- mersenne_twister.cpp
+++ mersenne_twister.cpp
@@ -3,7 +3,7 @@
 #include <random>
 
 std::vector<long double> getDecimalFractions(unsigned long n_samples) {
-    std::minstd_rand gen(time(nullptr));
+    std::mt19937_64 gen(time(nullptr));
     std::vector<long double> result(n_samples);
 
     for (unsigned long i = 0; i < n_samples; i++) {
@@ -15,7 +15,7 @@ std::vector<long double> getDecimalFractions(unsigned long n_samples) {
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        std::cout << "Usage: ./linear_congruential {n_samples}" << std::endl;
+        std::cout << "Usage: ./mersenne_twister {n_samples}" << std::endl;
         return 1;
     }
 
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
     std::vector<long double> result = getDecimalFractions(n_samples);
 
     std::cout << std::fixed << std::setprecision(20);
-    std::cout << "Linear Congruential Numbers" << std::endl;
+    std::cout << "Mersenne-Twister Numbers" << std::endl;
     for (const auto &num : result) {
         std::cout << num << std::endl;
     }
